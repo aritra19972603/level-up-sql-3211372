41b1e6ed8908844311a6a0495ea6f1429469dc80
aritra <49690310+aritra19972603@users.noreply.github.com>
2024-05-18 18:23:43 +0000

    add new customer

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,18 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT CustomerID, FirstName, LastName, Address 
+FROM Customers 
+WHERE FirstName = 'Taylor' 
+AND LastName = 'Jenkins';
+
+UPDATE Customers 
+SET Address = '74 Pine St.', 
+    City = 'New York', 
+    State = 'NY' 
+WHERE CustomerID = 26;
+
+SELECT * 
+FROM Customers 
+WHERE CustomerID = 26;
