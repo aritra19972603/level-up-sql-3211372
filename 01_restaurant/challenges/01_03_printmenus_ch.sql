ce4fbebfdde8be9c994204e1ede7d02e8be60f7b
aritra <49690310+aritra19972603@users.noreply.github.com>
2024-05-18 18:15:17 +0000

    create table

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -1,5 +1,11 @@
 -- Create reports that will be used to make three menus.
 
+SELECT Name,Type,Price,Description FROM Dishes ORDER by price; 
+
 -- Create a report with all the items sorted by price (lowest to highest).
+
+SELECT Name,Type,Price,Description FROM Dishes ORDER by price DESC; 
 -- Create a report showing appetizers and beverages.
+SELECT * from Dishes WHERE Type='Appetizer' and Type= 'Beverage' Order by Type;
 -- Create a report with all items except beverages.
+SELECT * from Dishes WHERE Type!= 'Beverage' Order by Type;
